205fbaad3dd80f98942807aac9666d252e44ac73
nakisa84 <nakisa.abbasi@gmail.com>
2022-02-22 17:59:29 -0500

    update some codes

--- notebooks/library/utils.py
+++ notebooks/library/utils.py
@@ -34,23 +34,9 @@ def save_file(data, fname, dname):
         print(f'Directory {dname} was created.')
         
     fpath = os.path.join(dname, fname)
-    
-    
+     
     if os.path.exists(fpath):
         print("A file already exists with this name.\n")
-
-        yesno = None
-        while yesno != "Y" and yesno != "N":
-            yesno = input('Do you want to overwrite? (Y/N)').strip()[0].capitalize()
-            if yesno == "Y":
-                print(f'Writing file.  "{fpath}"')
-                _save_file(data, fpath)
-                break  # Not required
-            elif yesno == "N":
-                print('\nPlease re-run this cell with a new filename.')
-                break  # Not required
-            else:
-                print('\nUnknown input, please enter "Y" or "N".')
 
     else:  # path does not exist, ok to save the file
         print(f'Writing file.  "{fpath}"')
